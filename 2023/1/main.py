43bdf6d90a54d5531fb8b3fad5a1bb4e4a37b700
Will Walker <w_walker@icloud.com>
2023-12-01 17:15:59 -0600

    complete day one

--- 2023/1/main.py
+++ 2023/1/main.py
@@ -1,6 +1,39 @@
+import re
+
+# Part 2...
+number_mapping = {
+	"one": "o1e",
+	"two": "t2o",
+	"three": "t3e",
+	"four": "f4r",
+	"five": "f5e",
+	"six": "s6x",
+	"seven": "s7n",
+	"eight": "e8t",
+	"nine": "n9e",
+}
+
+# The answer
+answer = 0
+
 # Read the file
 input = open('input.txt', 'r')
 lines = input.readlines()
 
+# For each line in the file...
 for line in lines:
-	print("{}".format(line.strip()))
+
+	# replace any instance of written digits
+	for key,value in number_mapping.items():
+		line = line.replace(key, value)
+
+	# strip all non-digit characters
+	line = [x for x in line if x.isnumeric()]
+
+	# add the first and last digit
+	line = line[0] + line[-1]
+	
+	# add the result to the answer global variable
+	answer += int(line)
+
+print("{}".format(answer))
